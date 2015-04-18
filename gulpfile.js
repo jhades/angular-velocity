8ea782fbc10041b91cad0548708ca9eabf8fa688
JHades <blogger@example.com>
2015-04-18 22:17:38 +0200

    prepare playground for angular 2 upgrade

--- gulpfile.js
+++ gulpfile.js
@@ -162,7 +162,7 @@ gulp.task('webserver', function() {
 // launch a build upon modification and publish it to a running server
 //
 /////////////////////////////////////////////////////////////////////////////////////
-gulp.task('dev', ['watch', 'webserver']);
+gulp.task('dev-old', ['watch', 'webserver']);
 
 /////////////////////////////////////////////////////////////////////////////////////
 //
@@ -170,3 +170,101 @@ gulp.task('dev', ['watch', 'webserver']);
 //
 /////////////////////////////////////////////////////////////////////////////////////
 gulp.task('default', ['sprite','build']);
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// ANGULAR 2 SECTION
+//
+/////////////////////////////////////////////////////////////////////////////////////
+
+var del = require('del');
+var plumber = require('gulp-plumber');
+var rename = require('gulp-rename');
+var traceur = require('gulp-traceur');
+
+var PATHS = {
+    src: {
+        js: ['ngv2/**/*.js', 'showcase/**/*.js'],
+        html: ['ngv2/**/*.html', 'showcase/**/*.html']
+    },
+    lib: [
+        'node_modules/gulp-traceur/node_modules/traceur/bin/traceur-runtime.js',
+        'node_modules/es6-module-loader/dist/es6-module-loader-sans-promises.src.js',
+        'node_modules/systemjs/lib/extension-register.js',
+        'node_modules/angular2/node_modules/zone.js/zone.js',
+        'node_modules/angular2/node_modules/zone.js/long-stack-trace-zone.js'
+    ]
+};
+
+gulp.task('clean', function(done) {
+    del(['dist'], done);
+});
+
+gulp.task('js', function () {
+    return gulp.src(PATHS.src.js)
+        .pipe(rename({extname: ''})) //hack, see: https://github.com/sindresorhus/gulp-traceur/issues/54
+        .pipe(plumber())
+        .pipe(traceur({
+            modules: 'instantiate',
+            moduleName: true,
+            annotations: true,
+            types: true,
+            memberVariables: true
+        }))
+        .pipe(rename({extname: '.js'})) //hack, see: https://github.com/sindresorhus/gulp-traceur/issues/54
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('html', function () {
+    return gulp.src(PATHS.src.html)
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('libs', ['angular2'], function () {
+    var size = require('gulp-size');
+    return gulp.src(PATHS.lib)
+        .pipe(size({showFiles: true, gzip: true}))
+        .pipe(gulp.dest('dist/lib'));
+});
+
+gulp.task('angular2', function () {
+
+    var buildConfig = {
+        paths: {
+            "angular2/*": "node_modules/angular2/es6/prod/*.es6",
+            "rx/*": "node_modules/angular2/node_modules/rx/*.js"
+        }
+    };
+
+    var Builder = require('systemjs-builder');
+    var builder = new Builder(buildConfig);
+
+    return builder.build('angular2/angular2', 'dist/lib/angular2.js', {});
+});
+
+gulp.task('dev', ['build'], function () {
+
+    var http = require('http');
+    var connect = require('connect');
+    var serveStatic = require('serve-static');
+    var open = require('open');
+
+    var port = 9000, app;
+
+    gulp.watch(PATHS.src.html, ['html']);
+    gulp.watch(PATHS.src.js, ['js']);
+
+    app = connect().use(serveStatic(__dirname + '/dist'));  // serve everything that is static
+    http.createServer(app).listen(port, function () {
+        open('http://localhost:' + port + '/showcase/index.html');
+    });
+});
+
+gulp.task('build', ['js', 'html', 'libs']);
+
+
+
+
+
+
+
