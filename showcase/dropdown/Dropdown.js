ca28f71d88b0efde89dfbbc75ffe28fe73e74176
JHades <blogger@example.com>
2015-04-19 22:58:40 +0200

    dropdown Angular 2

--- showcase/dropdown/Dropdown.js
+++ showcase/dropdown/Dropdown.js
@@ -1,5 +1,28 @@
 import {Component,Template, View, bootstrap} from 'angular2/angular2';
 
+
+@Component({
+    selector: 'ngv-selection-list'
+})
+@View({
+    template: `
+                <div class="selection-list" style="max-height:100px;width:280px">
+                    <div class="selection-option">
+                        <div class="selection-description">Option 1</div>
+                    </div>
+                    <div class="selection-option selected">
+                        <div class="selection-description">Option 2</div>
+                    </div>
+                    <div class="selection-option">
+                        <div class="selection-description">Option 3</div>
+                    </div>
+                </div>`
+})
+class SelectionList {
+
+
+}
+
 @Component({
     selector: 'ngv-dropdown'
 })
@@ -7,20 +30,10 @@ import {Component,Template, View, bootstrap} from 'angular2/angular2';
     template: ` <div class="ngv-input select-one clearfix">
                     <input type="text">
                     <div class="widget-button dropdown-button active"></div>
+                    <ngv-selection-list></ngv-selection-list>
 
-                    <div class="selection-list" style="max-height:100px;width:280px">
-
-                        <div class="selection-option">
-                            <div class="selection-description">Option 1</div>
-                        </div>
-                        <div class="selection-option selected">
-                            <div class="selection-description">Option 2</div>
-                        </div>
-                        <div class="selection-option">
-                            <div class="selection-description">Option 3</div>
-                        </div>
-                    </div>
-                </div>`
+                </div>`,
+    directives: [SelectionList]
 })
 export class Dropdown {
 
