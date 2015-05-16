6e8b910d1353103ea846fb6c4a41e809dd37f6a4
JHades <blogger@example.com>
2015-05-16 22:39:45 +0200

    dropdown ongoing

--- showcase/common/KeyboardUtils.js
+++ showcase/common/KeyboardUtils.js
@@ -15,4 +15,52 @@ export class KeyboardUtils {
         return keyCode >= 37 && keyCode <= 40;
     }
 
+    isArrowUp(keyCode) {
+        return keyCode == 38;
+    }
+
+    isArrowDown(keyCode) {
+        return keyCode == 40;
+    }
+
+    isArrowLeft(keyCode) {
+        return keyCode == 37;
+    }
+
+    isArrowRight(keyCode) {
+        return keyCode == 39;
+    }
+
+    isEnter(keyCode) {
+        return keyCode == 13;
+    }
+
+    isTab(keyCode) {
+        return keyCode == 9;
+    }
+
+    isEsc(keyCode) {
+        return keyCode == 27;
+    }
+
+    isShift(keyCode) {
+        return keyCode == 16;
+    }
+
+    isAlt(keyCode) {
+        return keyCode == 18;
+    }
+
+    isCtrl(keyCode) {
+        return keyCode == 17;
+    }
+
+    isMeta(keyCode) {
+        return this.isAlt(keyCode) || this.isCtrl(keyCode) || keyCode == 91;
+    }
+
 }
+
+
+
+
