57733174731c40bd6bb47ded3ce59a4b40b58f7a
JHades <blogger@example.com>
2015-05-25 23:12:36 +0200

    angular 2 setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,259 +1,73 @@
+'use strict';
 
-var gulp = require('gulp'),
-    webserver = require('gulp-webserver'),
-    sass = require('gulp-sass'),
-    jshint = require('gulp-jshint'),
-    sourcemaps = require('gulp-sourcemaps'),
-    spritesmith = require('gulp.spritesmith'),
-    browserify = require('browserify'),
-    source = require('vinyl-source-stream'),
-    buffer = require('vinyl-buffer'),
-    uglify = require('gulp-uglify'),
-    gutil = require('gulp-util');
-
-var CacheBuster = require('gulp-cachebust');
-var cachebust = new CacheBuster();
-
-/////////////////////////////////////////////////////////////////////////////////////
-//
-// runs bower to install frontend dependencies
-//
-/////////////////////////////////////////////////////////////////////////////////////
-gulp.task('install', function() {
-
-    var install = require("gulp-install");
-
-    return gulp.src(['./bower.json'])
-        .pipe(install());
-});
-
-/////////////////////////////////////////////////////////////////////////////////////
-//
-// generates a sprite png and the corresponding sass sprite map.
-// This is not included in the recurring development build and needs to be run separately
-//
-/////////////////////////////////////////////////////////////////////////////////////
-gulp.task('sprite', function () {
-    
-    var spriteData = gulp.src('./ngv/images/sprite/*.png')
-        .pipe(spritesmith({
-            imgName: 'angular-velocity-sprite.png',
-            cssName: '_angular-velocity-sprite.scss',
-            algorithm: 'top-down',
-            padding: 5
-        }));
-    
-        spriteData.css.pipe(gulp.dest('./ngv/styles/project'));
-
-        spriteData.img.pipe(gulp.dest('./dist'))
-});
-
-/////////////////////////////////////////////////////////////////////////////////////
-//
-// runs sass, creates css source maps
-//
-/////////////////////////////////////////////////////////////////////////////////////
-gulp.task('build-css', ['install'], function() {
-    return gulp.src('./ngv/styles/**/*.*css')
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(cachebust.resources())
-        .pipe(sourcemaps.write('./maps'))
-        .pipe(gulp.dest('./dist'));
-});
-
-/////////////////////////////////////////////////////////////////////////////////////
-//
-// fills in the Angular template cache, to prevent loading html template via multiple
-// http requests
-//
-/////////////////////////////////////////////////////////////////////////////////////
-gulp.task('build-template-cache',  function() {
-    
-    var ngHtml2Js = require("gulp-ng-html2js"),
-        concat = require("gulp-concat");
-    
-    return gulp.src("./ngv/partials/*.html")
-        .pipe(ngHtml2Js({
-            moduleName: "ngvPartials",
-            prefix: "/ngv/partials/"
-        }))
-        .pipe(concat("templateCachePartials.js"))
-        .pipe(gulp.dest("./dist"));
-});
-
-/////////////////////////////////////////////////////////////////////////////////////
-//
-// runs jshint
-//
-/////////////////////////////////////////////////////////////////////////////////////
-
-gulp.task('jshint', function() {
-    gulp.src('/ngv/directives/**/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
-});
-
-/////////////////////////////////////////////////////////////////////////////////////
-//
-// TODO
-//
-/////////////////////////////////////////////////////////////////////////////////////
-
-gulp.task('build-js', function() {
-    var b = browserify({
-        entries: './demo/run-demo-app.js',
-        debug: true,
-        paths: ['/demo/scripts','./ngv','/ngv/directives']
-    });
-
-    return b.bundle()
-        .pipe(source('bundle.js'))
-        .pipe(buffer())
-        .pipe(cachebust.resources())
-        .pipe(sourcemaps.init({loadMaps: true}))
-        /*.pipe(uglify())*/
-        .on('error', gutil.log)
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./dist/js/'));
-});
-
-/////////////////////////////////////////////////////////////////////////////////////
-//
-// full build (except sprites), applies cache busting to the main page css references
-//
-/////////////////////////////////////////////////////////////////////////////////////
-gulp.task('build', ['install','build-css','build-template-cache', 'jshint', 'build-js'], function() {
-    return gulp.src('showcase.html')
-        .pipe(cachebust.references())
-        .pipe(gulp.dest('dist'));
-});
-
-/////////////////////////////////////////////////////////////////////////////////////
-//
-// watches the main file types, and triggers a build when a modification is detected
-//
-/////////////////////////////////////////////////////////////////////////////////////
-gulp.task('watch', function() {
-    gulp.watch([
-        './ngv/partials/*.html',
-        './ngv/styles/**/*.*css',
-        './demo/forms/*.html', ', ',
-        './ngv/directives/*.js'], ['build']);
-});
-
-/////////////////////////////////////////////////////////////////////////////////////
-//
-// launches a web server that serves files in the current directory
-//
-/////////////////////////////////////////////////////////////////////////////////////
-gulp.task('webserver', function() {
-    gulp.src('.')
-        .pipe(webserver({
-            livereload: true,
-            port:9000,
-            directoryListing: true,
-            open: "http://localhost:9000/dist/showcase.html"
-        }));
-});
-
-/////////////////////////////////////////////////////////////////////////////////////
-//
-// launch a build upon modification and publish it to a running server
-//
-/////////////////////////////////////////////////////////////////////////////////////
-gulp.task('dev-old', ['watch', 'webserver']);
-
-/////////////////////////////////////////////////////////////////////////////////////
-//
-// installs and builds everything, including sprites
-//
-/////////////////////////////////////////////////////////////////////////////////////
-gulp.task('default', ['sprite','build']);
-
-/////////////////////////////////////////////////////////////////////////////////////
-//
-// ANGULAR 2 SECTION
-//
-/////////////////////////////////////////////////////////////////////////////////////
-
+var gulp = require('gulp');
+var del = require('del');
+var Builder = require('systemjs-builder');
+var ts = require('gulp-typescript');
 var plumber = require('gulp-plumber');
-var rename = require('gulp-rename');
-var traceur = require('gulp-traceur');
+var sourcemaps = require('gulp-sourcemaps');
 
-var PATHS = {
-    src: {
-        js: ['ngv2/**/*.js', 'showcase/**/*.js']
-    }
-};
+var http = require('http');
+var connect = require('connect');
+var serveStatic = require('serve-static');
+var openResource = require('open');
 
-gulp.task('clean', function(done) {
+gulp.task('clean', function (done) {
     del(['dist'], done);
 });
 
-gulp.task('angular2', function () {
-
-    var buildConfig = {
+gulp.task('build:angular2', function () {
+    var builder = new Builder({
         paths: {
-            "angular2/*": "node_modules/angular2/es6/prod/*.es6",
-            "rx": "node_modules/angular2/node_modules/rx/dist/rx.js"
+            'angular2/*': 'node_modules/angular2/es6/prod/*.es6',
+            rx: 'node_modules/angular2/node_modules/rx/dist/rx.js'
         },
         meta: {
-           // auto-detection fails to detect properly here - https://github.com/systemjs/builder/issues/123
-           'rx': {
-               format: 'cjs'
-           }
-        }        
-    };
-
-    var Builder = require('systemjs-builder');
-    var builder = new Builder(buildConfig);
-
-    return builder.build('angular2/angular2', 'dist/lib/angular2.js', {});
+            rx: {
+                format: 'cjs'
+            }
+        }
+    });
+    return builder.build('angular2/angular2', './dist/lib/angular2.js', {});
 });
 
-gulp.task('es6', ['angular2'], function () {
-    return gulp.src(PATHS.src.js)
-        .pipe(rename({extname: ''})) //hack, see: https://github.com/sindresorhus/gulp-traceur/issues/54
+gulp.task('build:lib', ['build:angular2'], function () {
+    gulp.src([
+        './node_modules/angular2/node_modules/traceur/bin/traceur-runtime.js',
+        './node_modules/angular2/node_modules/zone.js/dist/zone.js',
+        './node_modules/es6-module-loader/dist/es6-module-loader-sans-promises.js',
+        './node_modules/es6-module-loader/dist/es6-module-loader-sans-promises.js.map',
+        './node_modules/reflect-metadata/Reflect.js',
+        './node_modules/reflect-metadata/Reflect.js.map',
+        './node_modules/systemjs/dist/system.js',
+        './node_modules/systemjs/dist/system.js.map'
+    ])
+        .pipe(gulp.dest('./dist/lib'));
+});
+
+var tsProject = ts.createProject('tsconfig.json', {
+    typescript: require('typescript')
+});
+
+gulp.task('build', ['clean'], function () {
+    var result = gulp.src('./modules/**/*.ts')
         .pipe(plumber())
-        .pipe(traceur({
-            modules: 'instantiate',
-            moduleName: true,
-            annotations: true,
-            types: true,
-            memberVariables: true
-        }))
-        .pipe(rename({extname: '.js'})) //hack, see: https://github.com/sindresorhus/gulp-traceur/issues/54
-        .pipe(gulp.dest('dist'));
+        .pipe(sourcemaps.init())
+        .pipe(ts(tsProject));
+
+    return result.js
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('build-ngv2', ['build-css', 'es6'], function() {
-    return gulp.src('./showcase/index.html')
-        .pipe(cachebust.references())
-        .pipe(gulp.dest('dist'));
+gulp.task('serve', ['build:lib', 'build'], function () {
+    var port = 5555;
+    var app;
+
+    gulp.watch('./ts/**', ['build']);
+
+    app = connect().use(serveStatic(__dirname));
+    http.createServer(app).listen(port, function () {
+        openResource('http://localhost:' + port);
+    });
 });
-
-gulp.task('watch/ngv2', function() {
-    gulp.watch([
-        './ngv/styles/**/*.*css',
-        './ngv2/**/*.html',
-        './ngv2/**/*.js',
-        './showcase/**/*.js',
-        './showcase/**/*.html'], ['build-ngv2']);
-});
-
-gulp.task('serve/ngv2', function() {
-    gulp.src('.')
-        .pipe(webserver({
-            livereload: true,
-            port:9001,
-            directoryListing: true,
-            open: "http://localhost:9001/dist/index.html"
-        }));
-});
-
-gulp.task('ngv2', ['watch/ngv2', 'serve/ngv2']);
-
-
-
-
