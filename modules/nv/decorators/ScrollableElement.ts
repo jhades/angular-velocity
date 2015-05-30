10fd9603874c865c0f7eccf79d19096da6e8f71b
JHades <blogger@example.com>
2015-05-30 11:39:58 +0200

    dropdown ongoing

--- modules/nv/decorators/ScrollableElement.ts
+++ modules/nv/decorators/ScrollableElement.ts
@@ -1,4 +1,4 @@
-import {Directive, Ancestor} from 'angular2/angular2';
+import {Directive, Ancestor, ElementRef} from 'angular2/angular2';
 import {Scrollable} from 'nv/decorators';
 import {Highlightable} from 'nv/core/Highlightable';
 
@@ -15,9 +15,11 @@ import {Highlightable} from 'nv/core/Highlightable';
 export class ScrollableElement {
     highlightable:Highlightable;
     scrollable: Scrollable;
+    el: ElementRef;
 
-    constructor(@Ancestor(Scrollable) scrollable: Scrollable) {
+    constructor(@Ancestor(Scrollable) scrollable: Scrollable, el: ElementRef) {
         this.scrollable = scrollable;
+        this.el = el;
         this.scrollable.addScrollableElement(this);
     }
 
