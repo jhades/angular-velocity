7f6c577b225e8bbe98de2a25c065a03f385c2d14
JHades <blogger@example.com>
2015-06-01 22:43:20 +0200

    dropdown ongoing

--- modules/nv/components/selectone/SelectionOption.ts
+++ modules/nv/components/selectone/SelectionOption.ts
@@ -2,14 +2,12 @@ import {Highlightable} from 'nv/core/Highlightable';
 
 export interface SelectionOption extends Highlightable {
     description: string;
+    disabled: boolean;
 }
 
 
 export class BlankOption implements SelectionOption {
-    description:string;
-    highlighted:boolean;
-    constructor() {
-        this.description = "";
-        this.highlighted = false;
-    }
+    description:string = "";
+    highlighted:boolean = false;
+    disabled: boolean = false;
 }
