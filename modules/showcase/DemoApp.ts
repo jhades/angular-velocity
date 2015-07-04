cfcb314262c24f672d332f989d157a1263e657c2
JHades <blogger@example.com>
2015-07-04 10:14:41 +0200

    dropdown ongoing

--- modules/showcase/DemoApp.ts
+++ modules/showcase/DemoApp.ts
@@ -14,7 +14,7 @@ import {Dropdown} from 'nv/components/dropdown/Dropdown';
 
                         <form class="pure-form">
                             <h2>Dropdown</h2>
-                            <nv-dropdown num-visible-options="7" dropdown-width="'200px'"
+                            <nv-dropdown num-visible-options="7" dropdown-width="200px"
                                 [options]="refData.COUNTRIES"
                                 (change)="onSelection($event)">
                             </nv-dropdown>
