4af00d3703ec49febf3cba97df36f3b1850c4ac6
JHades <blogger@example.com>
2015-07-04 10:00:31 +0200

    dropdown ongoing

--- modules/nv/decorators.ts
+++ modules/nv/decorators.ts
@@ -1,6 +1,6 @@
 import {ScrollableList} from 'nv/decorators/scrollablelist/ScrollableList';
 import {ScrollableListElement} from 'nv/decorators/scrollablelist/ScrollableListElement';
-import {TypeSearch} from 'nv/decorators/TypeSearch';
+import {TypeSearch} from 'nv/decorators/typesearch/TypeSearch';
 
 export {
     ScrollableList,
