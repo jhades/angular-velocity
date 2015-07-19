7a950977b985a4235c311818531ca728eb2770a3
JHades <jhades.dev@gmail.com>
2015-07-19 20:07:00 +0200

    debounced mousewheel on selection lists for smoother scrolling

--- modules/nv/services/FunctionalUtils.ts
+++ modules/nv/services/FunctionalUtils.ts
@@ -4,10 +4,17 @@ import {Injectable} from "angular2/di";
 @Injectable()
 export class FunctionalUtils {
 
-    // Returns a function, that, as long as it continues to be invoked, will not
-    // be triggered. The function will be called after it stops being called for
-    // N milliseconds. If `immediate` is passed, trigger the function on the
-    // leading edge, instead of the trailing.
+
+    /*
+     *
+     *  Returns a function, that, as long as it continues to be invoked, will not
+     *  be triggered. The function will be called after it stops being called for
+     *  N milliseconds. If `immediate` is passed, trigger the function on the
+     *  leading edge, instead of the trailing.
+     *
+     *  (taken from lodash -> http://davidwalsh.name/javascript-debounce-function)
+     *
+    */
     debounce(func, wait, immediate?) {
         var timeout;
         return function () {
