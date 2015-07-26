0414b8b207366659ad5e675d3ea104b2e2b49c61
JHades <jhades.dev@gmail.com>
2015-07-26 15:54:24 +0200

    tab container component

--- modules/nv/components/tabs/Tab.ts
+++ modules/nv/components/tabs/Tab.ts
@@ -1,6 +1,14 @@
 
+import {Directive, View} from 'angular2/angular2';
 
-
+@Directive({
+    selector: 'nv-tab'
+})
 export class Tab {
+
+    constructor() {
+        console.log("tab ");
+    }
+
 
 }
--- modules/nv/components/tabs/Tabs.ts
+++ modules/nv/components/tabs/Tabs.ts
@@ -2,6 +2,16 @@
 import {Component, View} from 'angular2/angular2';
 import {Tab} from './Tab';
 
+@Component({
+    selector: 'nv-tabs'
+})
+@View({
+    template: `Tabszzzzz`
+})
 export class Tabs {
+
+    constructor() {
+        console.log("Tabs ...");
+    }
 
 }
