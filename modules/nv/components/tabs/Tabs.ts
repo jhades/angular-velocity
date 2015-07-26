8a5e9346e28762043481127e00f48df46f008ba7
JHades <jhades.dev@gmail.com>
2015-07-26 16:17:00 +0200

    tab container component

--- modules/nv/components/tabs/Tabs.ts
+++ modules/nv/components/tabs/Tabs.ts
@@ -1,17 +1,38 @@
 
-import {Component, View} from 'angular2/angular2';
+import {Component, View, Query, QueryList, coreDirectives} from 'angular2/angular2';
 import {Tab} from './Tab';
 
 @Component({
     selector: 'nv-tabs'
 })
 @View({
-    template: `Tabszzzzz`
+    directives: [coreDirectives, Tab],
+    template: `<div class="tabcontainer">
+                <ul class="tabcontainer-buttons">
+                    <li *ng-for="#tab of tabs" class="selected">{{tab.label}}</li>
+                </ul>
+
+                <div class="tabcontainer-body">
+
+                    <content></content>
+
+                </div>`
 })
 export class Tabs {
 
-    constructor() {
-        console.log("Tabs ...");
+    tabsQuery: QueryList<Tab>;
+    tabs: Array<Tab>;
+
+    constructor(@Query(Tab) tabsQuery: QueryList<Tab>) {
+        this.tabsQuery = tabsQuery;
+        tabsQuery.onChange(() => this.onTabsChanged());
+    }
+
+    onTabsChanged() {
+        this.tabs= [];
+        for (let tab of this.tabsQuery._results) { //TODO refactor once Query list is an observable
+            this.tabs.push(tab);
+        }
     }
 
 }
