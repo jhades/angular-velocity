1190c2464083a9519d016a3e30901b7c6d76029f
JHades <jhades.dev@gmail.com>
2015-07-26 20:03:37 +0200

    collapsible panel

--- modules/nv/components/collapsiblepanel/CollapsiblePanel.ts
+++ modules/nv/components/collapsiblepanel/CollapsiblePanel.ts
@@ -4,6 +4,11 @@ import {Component, View, EventEmitter} from "angular2/angular2";
 @Component({
     selector:'nv-collapsible-panel'
 })
+@View({
+    templateUrl:'nv/components/collapsiblepanel/collapsible-panel.html'
+})
 export class CollapsiblePanel {
+
+
 
 }
