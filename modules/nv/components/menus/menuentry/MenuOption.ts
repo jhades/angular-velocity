063d97e1873f375a0e499741b63a2d40c5c2aa55
JHades <jhades.dev@gmail.com>
2015-07-27 17:26:38 +0200

    top menu

--- modules/nv/components/menus/menuentry/MenuOption.ts
+++ modules/nv/components/menus/menuentry/MenuOption.ts
@@ -1,20 +1,20 @@
 
-import {Directive, View, coreDirectives, Query, QueryList} from 'angular2/angular2';
+import {Directive, View, coreDirectives, Query, QueryList, Attribute} from 'angular2/angular2';
 
 @Directive({
     selector: 'nv-top-menu option'
 })
 export class MenuOption {
-
+    title: string;
     options: QueryList<MenuOption>;
 
-    constructor(@Query(MenuOption, {descendants:false}) options: QueryList<MenuOption>) {
+    constructor(@Query(MenuOption) options: QueryList<MenuOption>, @Attribute("title") title) {
         console.log("builded menu option");
         this.options = options;
+        this.title = title;
         this.options.onChange(() => {
             console.log('top menu entries ' + this.options.length);
         });
     }
-
 
 }
--- modules/nv/components/menus/topmenu/TopMenu.ts
+++ modules/nv/components/menus/topmenu/TopMenu.ts
@@ -1,24 +1,37 @@
 
 import {Component, View, coreDirectives, Query, QueryList} from 'angular2/angular2';
 import {MenuOption} from '../menuentry/MenuOption';
+import {MenuOptionGroup} from '../menuentry/MenuOptionGroup';
 
 @Component({
     selector: 'nv-top-menu'
 })
 @View({
-    directives: [coreDirectives, MenuOption],
+    directives: [coreDirectives],
     templateUrl: 'nv/components/menus/topmenu/top-menu.html'
 })
 export class TopMenu {
 
     options: QueryList<MenuOption>;
 
-    constructor(@Query(MenuOption, {descendants:false}) topMenuEntries: QueryList<MenuOption>) {
+    //TODO pending https://github.com/angular/angular/issues/3034
+    /**
+     *
+     * design: inject a list of both groups and options
+     *
+     */
+    constructor(@Query(MenuOption) topMenuEntries: QueryList<MenuOption>) {
         console.log("builded top menu");
         this.options = topMenuEntries;
         this.options.onChange(() => {
-           console.log('top menu entries ' + this.options.length);
+            console.log("received query results");
+           console.log(this.options);
         });
+    }
+
+
+    static queryOptionsAndOptionGroups(obj) {
+        return obj instanceof MenuOption || obj instanceof MenuOptionGroup;
     }
 
 
