5b362d3805ca4c129b617de8379b0fac1f235f39
JHades <jhades.dev@gmail.com>
2015-11-01 17:16:27 +0100

    ongoing

--- modules/nv/components/tabs/Tabs.ts
+++ modules/nv/components/tabs/Tabs.ts
@@ -1,7 +1,6 @@
 
 import {Component, View, Query, QueryList, CORE_DIRECTIVES} from 'angular2/angular2';
 import {Tab} from './Tab';
-import {ObservableWrapper} from 'angular2/src/core/facade/async';
 
 @Component({
     selector: 'nv-tabs'
@@ -17,12 +16,12 @@ export class Tabs {
 
     constructor(@Query(Tab) tabsQuery: QueryList<Tab>) {
         this.tabsQuery = tabsQuery;
-        ObservableWrapper.subscribe(tabsQuery.changes, () => this.onTabsChanged());
+        tabsQuery.changes.toRx().subscribe(() => this.onTabsChanged());
     }
 
     onTabsChanged() {
         this.tabs= [];
-        for (let tab of this.tabsQuery._results) { //TODO refactor once Query list is an observable
+        for (let tab of this.tabsQuery.toArray()) { //TODO refactor once Query list is an observable
             this.tabs.push(tab);
         }
         if (this.tabs.length > 0) {
